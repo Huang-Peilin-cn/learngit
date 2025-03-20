a7a926e6219f36b25717f696a6d7d91b12dd9420
mylocal <1741539548@qq.com>
2025-03-20 20:40:16 +0800

    2025_2

--- c.cpp
+++ c.cpp
@@ -3,6 +3,10 @@
 int main()
 {
     std::cout << 11 << std::endl;
+    for (int i = 0; i < 10; i++)
+    {
+        std::cout << i << std::endl;
+    }
     // Creating a new branch is quick.
     // Creating a new branch is quick and simple.
 }
